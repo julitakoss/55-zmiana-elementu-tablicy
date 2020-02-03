aabd4b0d3c70131a67c79a90096d0eef8e51e721
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-03 12:51:00 +0100

    dodanie projektu

---
+++ main.js
@@ -0,0 +1,13 @@
+const tab = [24,1,34];
+function syf(qr){
+    if (qr[0] > 10) {
+        qr[0] = 5;     
+    }
+    if (qr[1] > 10){
+        qr[1] = 5;
+    }
+    if (qr[2] > 10){
+        qr[2] = 5;
+    }
+    console.log(qr);
+}
